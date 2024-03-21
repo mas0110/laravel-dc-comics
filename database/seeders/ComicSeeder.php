e2c75a00ffe02d6af0051fb3eed396b1599d6359
mas0110 <masoudmohammed13@gmail.com>
2024-03-21 16:59:48 +0100

    creazione seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $comics = config('comics');
+
+        foreach ($comics as $comic){
+            $new_comic = new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->description = $comic['description'];
+            $new_comic->series = $comic['series'];
+            $new_comic->type = $comic['type'];
+            $new_comic->price = $comic['price'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->sale_date = $comic['sale_date'];
+
+            $new_comic->save();
+        }
     }
 }
