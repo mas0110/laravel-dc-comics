bb38cd01341979c8e2aa81ac0428f3e715dddb06
mas0110 <masoudmohammed13@gmail.com>
2024-03-21 17:03:47 +0100

    layout della pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
@@ -13,12 +13,12 @@
 </head>
 
 <body>
-    <header>
-        @include('components.header')
-    </header>
+    {{-- <header> --}}
+        {{-- @include('components.header') --}}
+    {{-- </header> --}}
 
     <main>
-        @yield('main_content')
+        @yield('main')
     </main>
 </body>
 
