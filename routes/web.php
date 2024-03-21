9aff417b078f67a016cb1a13186c3f72ff99528c
mas0110 <masoudmohammed13@gmail.com>
2024-03-21 17:01:59 +0100

    rotte comics e homepage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\backend\comicsController;
 use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PageController::class, 'index'])->name('HomePage');
 
 Route::get('/about', [PageController::class, 'indexAbout'])->name('AboutPage');
+
+Route::resource('comics', comicsController::class );
