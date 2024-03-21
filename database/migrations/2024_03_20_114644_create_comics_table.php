14e501ef80c34765c33b46e6a63e83c1f95e688d
mas0110 <masoudmohammed13@gmail.com>
2024-03-21 16:58:24 +0100

    crezione della migration

--- database/migrations/2024_03_20_114644_create_comics_table.php
+++ database/migrations/2024_03_20_114644_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+
+            $table->string("title", 50);
+            $table->text("description");
+            $table->string("thumb");
+            $table->decimal("price", 5, 2);
+            $table->string("series", 100);
+            $table->date("sale_date");
+            $table->string("type", 20);
+
             $table->timestamps();
         });
     }
