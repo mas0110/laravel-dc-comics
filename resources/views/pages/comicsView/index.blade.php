656f466b3305e513a56a4fe391885930e467705b
mas0110 <masoudmohammed13@gmail.com>
2024-03-22 14:32:11 +0100

    link per la pagina create dalla pagina index

--- resources/views/pages/comicsView/index.blade.php
+++ resources/views/pages/comicsView/index.blade.php
@@ -1,28 +1,33 @@
 @extends('layouts.app')
 
-@section('title', 'DC comics | read Index')
+@section('title', 'DC comics | Fumetti')
 
 @section('main')
+<div class="container">
     <h1>pagina dc comics</h1>
-        <div class="row p-3">
-        @foreach ($comics as $item)
-        <div class="col-3 py-5">
-            <div class="card bg-light text-dark" style="width: 18rem; min-height: 670px">
-                <img src="{{$item->thumb}}" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">{{$item->title}}</h5>
-                    {{-- <p class="card-text"> --}}
-                        {{-- {{$item->description}} --}}
-                    {{-- </p> --}}
-                    <div class="d-flex flex-column">
-                        <span><strong>Series:</strong> {{$item->series}}</span>
-                        <span>${{$item->price}}</span>
-                        <span><strong>Sala date: </strong>{{$item->sale_date}}</span>
-                        <span><strong>Type:</strong> {{$item->type}}</span>
+    <div class="btn btn-primary mt-2">
+        <a href="{{ route('comics.create')  }}">crea fumetto</a>
+    </div>
+            <div class="row p-3">
+            @foreach ($comics as $item)
+            <div class="col-3 py-5">
+                <div class="card bg-light text-dark" style="width: 18rem; min-height: 670px">
+                    <img src="{{$item->thumb}}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$item->title}}</h5>
+                        {{-- <p class="card-text"> --}}
+                            {{-- {{$item->description}} --}}
+                        {{-- </p> --}}
+                        <div class="d-flex flex-column">
+                            <span><strong>Series:</strong> {{$item->series}}</span>
+                            <span><strong>Price:</strong> ${{$item->price}}</span>
+                            <span><strong>Sala date:</strong> {{$item->sale_date}}</span>
+                            <span><strong>Type:</strong> {{$item->type}}</span>
+                        </div>
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
+</div>
 @endsection
